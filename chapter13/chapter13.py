621ea0131472f1a1775910bb3f3c1a2ca524d042
Lokesh <luckytammisetty@email.com>
2020-06-11 16:36:07 -0500

    Exercise

--- chapter13/chapter13.py
+++ chapter13/chapter13.py
@@ -1,6 +1,8 @@
 def func13_1(document):
-    doc = open(document, '+')
+    doc = open(document, 'r+')
     lines = doc.readlines()
-    print(lines)
+    newdoc = open('newdoc.txt', 'w')
+    for i in range(len(lines) - 1, -1, -1):
 
-func13_1()
+
+func13_1('chapter13text.txt')
