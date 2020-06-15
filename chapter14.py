3b4068dc2e96414bfff29def2ab8daedad6a8c19
Lokesh <luckytammisetty@email.com>
2020-06-15 16:55:29 -0500

    Exercise

--- chapter14.py
+++ chapter14.py
@@ -1,4 +1,83 @@
 import random
+from unit_tester import test
+import time
+
+
+def search_linear(xs, target):
+    """ Find and return the index of target in sequence xs """
+    for (i, v) in enumerate(xs):
+        if v == target:
+            return i
+    return -1
+
+
+def search_binary(xs, target):
+    """ Find and return the index of key in sequence xs """
+    lb = 0
+    ub = len(xs)
+    while True:
+        if lb == ub:   # If region of interest (ROI) becomes empty
+           return -1
+
+        # Next probe should be in the middle of the ROI
+        mid_index = (lb + ub) // 2
+
+        # Fetch the item at that position
+        item_at_mid = xs[mid_index]
+
+        print("ROI[{0}:{1}](size={2}), probed='{3}', target='{4}'".format(lb, ub, ub-lb, item_at_mid, target))
+
+        # How does the probed item compare to the target?
+        if item_at_mid == target:
+                return mid_index      # Found it!
+        elif item_at_mid < target:
+            lb = mid_index + 1    # Use upper half of ROI next time
+        else:
+            ub = mid_index        # Use lower half of ROI next time
+
+
+def find_unknown_words(vocab, wds):
+    """ Return a list of words in wds that do not occur in vocab """
+    result = []
+    for w in wds:
+        if (search_linear(vocab, w) < 0):
+            result.append(w)
+    return result
+
+
+def load_words_from_file(filename):
+    """ Read words from filename, return list of words. """
+    f = open(filename, "r")
+    file_content = f.read()
+    f.close()
+    wds = file_content.split()
+    return wds
+
+
+def text_to_words(the_text):
+    """ return a list of words with all punctuation removed,
+        and all in lowercase.
+    """
+
+    my_substitutions = the_text.maketrans(
+      # If you find any of these
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&()*+,-./:;<=>?@[]^_`{|}~'\\",
+      # Replace them by these
+      "abcdefghijklmnopqrstuvwxyz                                          ")
+
+    # Translate the text now.
+    cleaned_text = the_text.translate(my_substitutions)
+    wds = cleaned_text.split()
+    return wds
+
+
+def get_words_in_book(filename):
+    """ Read a book from filename, and return a list of its words. """
+    f = open(filename, "r")
+    content = f.read()
+    f.close()
+    wds = text_to_words(content)
+    return wds
 
 
 def merge(xs, ys, mode=0):
@@ -177,3 +256,13 @@ def prime_misses(draws):
     prime_in.sort()
     prime_ins = rem_adj_dup(prime_in)
     return [x for x in sieve(list(range(50))) if x not in prime_ins]
+
+
+book_words = get_words_in_book("AliceInWonderland.txt")
+bigger_vocab = load_words_from_file("vocab.txt")
+
+
+def test_suite():
+
+test_suite()
+
