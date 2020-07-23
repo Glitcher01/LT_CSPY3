87082bc0a8483197f65451ebbb6d8f4cea9a4574
Lokesh <luckytammisetty@email.com>
2020-07-23 16:21:19 -0500

    Exercise

--- chapter19.py
+++ chapter19.py
@@ -4,6 +4,8 @@ def readposint():
         val = int(num)
         if val <= 0:
             print("{0} is a integer, but isn't positive!".format(val))
+        else:
+            print('{0} is a positive integer'.format(val))
     except:
         if num == "":
             raise TypeError("You didn't type in anything!")
