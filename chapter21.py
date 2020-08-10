299f8d790305620257040aca724266faf185c72c
Lokesh <luckytammisetty@email.com>
2020-08-10 17:16:59 -0500

    Exercise

--- chapter21.py
+++ chapter21.py
@@ -1,9 +1,9 @@
 class MyTime:
     def __init__(self, hours=0, minutes=0, seconds=0):
         totalsecs = hours * 3600 + minutes * 60 + seconds
-        self.hours = totalsecs // 3600
-        leftsecs = totalsecs - hours * 3600
-        self.minutes, self.seconds = leftsecs // 60, leftsecs % 60
+        self.hours = int(totalsecs // 3600)
+        leftsecs = totalsecs - self.hours * 3600
+        self.minutes, self.seconds = int(leftsecs // 60), int(leftsecs % 60)
 
     def __add__(self, other):
         return MyTime(self.hours + other.hours, self.minutes + other.minutes, self.seconds + other.seconds)
@@ -12,7 +12,8 @@ class MyTime:
         return '{0}:{1}:{2}'.format(self.hours, self.minutes, self.seconds)
 
     def increment(self, secs):
-        final_time = self.__add__(MyTime(0, 0, secs))
+        final_secs = self.to_seconds() + secs
+        final_time = MyTime(0, 0, final_secs)
         self.hours, self.minutes, self.seconds = final_time.hours, final_time.minutes, final_time.seconds
 
     def to_seconds(self):
